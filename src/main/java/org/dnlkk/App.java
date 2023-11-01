5879c0cc5032b4d5d7f47903e0648aab9455c8b8
danilkk <ezzfvkoko@gmail.com>
2023-11-01 11:13:13 +0300

    feat: init app and test

---
+++ src/main/java/org/dnlkk/App.java
@@ -0,0 +1,10 @@
+package org.dnlkk;
+
+import com.dnlkk.DnlkkApplication;
+import com.dnlkk.boot.annotations.DnlkkWeb;
+
+@DnlkkWeb
+public class App 
+{
+    public static void main( String[] args ) { DnlkkApplication.run(App.class, args);}
+}
