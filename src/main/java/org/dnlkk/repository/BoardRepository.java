d67c9389635d1f9fcaf574f3b2c2bdca5dfebb43
danilkk <ezzfvkoko@gmail.com>
2023-11-10 15:30:43 +0300

    feat: get random board from all/one theme

--- src/main/java/org/dnlkk/repository/BoardRepository.java
+++ src/main/java/org/dnlkk/repository/BoardRepository.java
@@ -2,8 +2,12 @@ package org.dnlkk.repository;
 
 import com.dnlkk.dependency_injector.annotations.components.Repository;
 import com.dnlkk.repository.DnlkkRepository;
+import com.dnlkk.repository.Pageable;
 import org.dnlkk.model.Board;
 
 @Repository
 public interface BoardRepository extends DnlkkRepository<String, Board> {
+
+    Board find(Pageable pageable);
+    Board findByTheme(Integer themeId, Pageable pageable);
 }
---
+++ src/main/java/org/dnlkk/service/BoardService.java
@@ -0,0 +1,23 @@
+package org.dnlkk.service;
+
+import com.dnlkk.dependency_injector.annotations.AutoInject;
+import com.dnlkk.repository.Pageable;
+import org.dnlkk.model.Board;
+import org.dnlkk.repository.BoardRepository;
+
+public class BoardService {
+    @AutoInject
+    private BoardRepository boardRepository;
+
+    public Board getBoard(String id) {
+        return boardRepository.findById(id);
+    }
+
+    public Board getRandomBoardByThemeId(Pageable pageable, Integer themeId) {
+        return boardRepository.findByTheme(themeId, pageable);
+    }
+    public Board getRandomBoard(Pageable pageable) {
+        return boardRepository.find(pageable);
+    }
+
+}
